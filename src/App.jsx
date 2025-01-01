30f9e5f9958b1f34616c24d40230cba072bbc286
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2025-01-01 12:48:11 +0530

    Feat: Added responsiveness to the nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import { useState } from 'react'
 import './App.css'
+import { IoCloseSharp, IoMenuSharp} from "react-icons/io5";
 
 function App() {
-  const [mode, setMode] = useState("light")
+  const [dark, setDark] = useState(false)
+  const [menu, setMenu] = useState(false)
 
   const toggleTheme = () => {
-    setMode(mode === "dark" ? "light" : "dark")
+    setDark(!dark)
+  }
+
+  const toggleMenu = () => {
+    setMenu(!menu)
   }
   return (
-    <div className={`${mode}`}>
+    <div className={`${dark && "dark"}`}>
     <div className='bg-gray-200 dark:bg-black text-gray-600 min-h-screen dark:text-white'>
        <header className='bg-gray-200 text-gray-600  dark:bg-black dark:text-white font-bold text-xl w-full py-10'>
-        <nav>
-          <ul className='flex justify-end gap-10 pr-10'>
+        <nav className='hidden sm:block'>
+          <ul className='flex justify-end gap-4 pr-10 lg:gap-10'>
             <li className='font-medium hover:cursor-pointer'> Home</li>
             <li className='font-medium hover:cursor-pointer'> Contact</li>
             <li className='font-medium hover:cursor-pointer'> About</li>
             <li className='font-medium hover:cursor-pointer'> Categories</li>
-            <li className='font-medium hover:cursor-pointer'> <button onClick={toggleTheme}>Toggle Theme</button> </li>
-            
+            <li className='font-medium hover:cursor-pointer'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
           </ul>
         </nav>
+        <span className='flex justify-end pr-10 sm:hidden' onClick={toggleMenu}>{menu ? <IoCloseSharp className='w-10 h-10'/> : <IoMenuSharp className='w-10 h-10'/>}</span>
+        {
+          menu && <nav className='mt-5 sm:hidden'>
+          <ul className='flex flex-col gap-10 items-end pr-20 lg:gap-10'>
+            <li className='font-medium hover:cursor-pointer'> Home</li>
+            <li className='font-medium hover:cursor-pointer'> Contact</li>
+            <li className='font-medium hover:cursor-pointer'> About</li>
+            <li className='font-medium hover:cursor-pointer'> Categories</li>
+            <li className='font-medium hover:cursor-pointer'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
+          </ul>
+        </nav>
+        }
        </header>
 
     </div>
