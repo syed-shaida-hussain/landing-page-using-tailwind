d01f48f618c747ddf45e7e07af31960f81ecfd5c
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2025-01-01 13:03:13 +0530

    Added a single classname to nav-links

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,22 @@ function App() {
        <header className='bg-gray-200 text-gray-600  dark:bg-black dark:text-white font-bold text-xl w-full py-10'>
         <nav className='hidden sm:block'>
           <ul className='flex justify-end gap-4 pr-10 lg:gap-10'>
-            <li className='font-medium hover:cursor-pointer'> Home</li>
-            <li className='font-medium hover:cursor-pointer'> Contact</li>
-            <li className='font-medium hover:cursor-pointer'> About</li>
-            <li className='font-medium hover:cursor-pointer'> Categories</li>
-            <li className='font-medium hover:cursor-pointer'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
+            <li className='nav-link'> Home</li>
+            <li className='nav-link'> Contact</li>
+            <li className='nav-link'> About</li>
+            <li className='nav-link'> Categories</li>
+            <li className='nav-link'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
           </ul>
         </nav>
         <span className='flex justify-end pr-10 sm:hidden' onClick={toggleMenu}>{menu ? <IoCloseSharp className='w-10 h-10'/> : <IoMenuSharp className='w-10 h-10'/>}</span>
         {
           menu && <nav className='mt-5 sm:hidden'>
           <ul className='flex flex-col gap-10 items-end pr-20 lg:gap-10'>
-            <li className='font-medium hover:cursor-pointer'> Home</li>
-            <li className='font-medium hover:cursor-pointer'> Contact</li>
-            <li className='font-medium hover:cursor-pointer'> About</li>
-            <li className='font-medium hover:cursor-pointer'> Categories</li>
-            <li className='font-medium hover:cursor-pointer'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
+            <li className='nav-link'> Home</li>
+            <li className='nav-link'> Contact</li>
+            <li className='nav-link'> About</li>
+            <li className='nav-link'> Categories</li>
+            <li className='nav-link'> <button onClick={toggleTheme}>{dark ? "Light Mode" : "Dark Mode"}</button> </li>
           </ul>
         </nav>
         }
